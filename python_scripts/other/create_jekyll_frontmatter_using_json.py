31f1797de0bc61f004da7f9d943f6b2b76eed649
dhmudalige <e18227@eng.pdn.ac.lk>
2022-07-06 11:21:15 +0530

    Update create_jekyll_frontmatter_using_json.json

--- python_scripts/other/create_jekyll_frontmatter_using_json.py
+++ python_scripts/other/create_jekyll_frontmatter_using_json.py
@@ -1,7 +1,7 @@
 # Author: E/18/227 Dinuka Mudalige - e18227@eng.pdn.ac.lk
 
 # import os
-import json # to get data from _data/semester#.json
+import json # to get data from _data/courses/semester#.json
 # import requests
 
 if __name__ == "__main__":
@@ -12,26 +12,35 @@ if __name__ == "__main__":
     # jsonPath = f"../_data/courses/semester1.json"
     # dataInJSON = json.load(open(jsonPath))
     dataInJSON = json.load(open("_data/courses/semester1.json"))
-    
-    for thisCourse in dataInJSON:
-        print(thisCourse)
-        
-    #     outputString = f"""---
-    # layout: courses
-    # permalink: "/courses/{semester}/{course_code.lower()}/"
-    # title: {courseData[COURSE_NAME]}
 
-    # course_number : {course_code.upper()} 
-    # course_title : {courseData[COURSE_NAME]}
-    # credits : {}
-    # core_or_elective : {} 
-    # prerequisites : {}
-    # aims_objectives : {}
-    # ilos_knowledge : {}
-    # ilos_skill : {}
-    # ilos_attitude : {}
-    # textbooks_references : {}
-    # ---"""
+    for thisCourse in dataInJSON:
+        # print(thisCourse)
+        # course_code = thisCourse["code"]
+        # course_title = thisCourse["name"]
+        # course_credits = thisCourse["credits"]
+        # core_or_elective = thisCourse["core/elective"]
+        # prerequisties = thisCourse["prerequisites"]
+        # ilos_knowledge = thisCourse["ILOs"]["Knowledge"]
+        # ilos_skill = thisCourse["ILOs"]["Skill"]
+        # ilos_attitude = thisCourse["ILOs"]["Attitude"]
+        # textbooks_references = thisCourse["references"]
+        
+        outputString = f"""---
+layout: courses
+permalink: "/courses/semester1/{thisCourse["code"].lower()}/"
+title: {thisCourse["name"]}
+
+course_number : {thisCourse["code"].upper()} 
+course_title : {thisCourse["name"]}
+credits : {thisCourse["credits"]}
+core_or_elective : {thisCourse["core/elective"]} 
+prerequisites : {thisCourse["prerequisites"]}
+ilos_knowledge : {thisCourse["ILOs"]["Knowledge"]}
+ilos_skill : {thisCourse["ILOs"]["Skill"]}
+ilos_attitude : {thisCourse["ILOs"]["Attitude"]}
+textbooks_references : {thisCourse["references"]}
+---"""
+        print(outputString)
 
     #     # write to html file
     #     file_url = "../"+f"pages/courses/{semester}/{course_code.lower()}.html"
