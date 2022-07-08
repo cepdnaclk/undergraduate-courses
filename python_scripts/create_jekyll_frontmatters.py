c3b9915e41c575fd1323ac653255d657ebccbecf
dhmudalige <e18227@eng.pdn.ac.lk>
2022-07-08 19:35:04 +0530

    Update create_jekyll_frontmatters.py

--- python_scripts/create_jekyll_frontmatters.py
+++ python_scripts/create_jekyll_frontmatters.py
@@ -13,34 +13,36 @@ if __name__ == "__main__":
     # dataInJSON = json.load(open(jsonPath))
     dataInJSON = json.load(open("_data/courses/semester3.json"))
 
-    for thisCourse in dataInJSON:
-        # print(thisCourse)
-        course_code = thisCourse["code"]
-        # course_title = thisCourse["name"]
-        # course_credits = thisCourse["credits"]
-        # core_or_elective = thisCourse["core/elective"]
-        # prerequisties = thisCourse["prerequisites"]
-        # ilos_knowledge = thisCourse["ILOs"]["Knowledge"]
-        # ilos_skill = thisCourse["ILOs"]["Skill"]
-        # ilos_attitude = thisCourse["ILOs"]["Attitude"]
-        # textbooks_references = thisCourse["references"]
-        
-        outputString = f"""---
-layout: courses
-permalink: "/courses/semester2/{thisCourse["code"].lower()}/"
-title: {thisCourse["name"]}
+    print(type(dataInJSON[0]))
 
-course_number : {thisCourse["code"].upper()} 
-course_title : {thisCourse["name"]}
-credits : {thisCourse["credits"]}
-core_or_elective : {thisCourse["core/elective"]} 
-prerequisites : {thisCourse["prerequisites"]}
-ilos_knowledge : {thisCourse["ILOs"]["Knowledge"]}
-ilos_skill : {thisCourse["ILOs"]["Skill"]}
-ilos_attitude : {thisCourse["ILOs"]["Attitude"]}
-textbooks_references : {thisCourse["references"]}
----"""
-        # print(outputString)
+#     for thisCourse in dataInJSON:
+#         # print(thisCourse)
+#         course_code = thisCourse["code"]
+#         # course_title = thisCourse["name"]
+#         # course_credits = thisCourse["credits"]
+#         # core_or_elective = thisCourse["core/elective"]
+#         # prerequisties = thisCourse["prerequisites"]
+#         # ilos_knowledge = thisCourse["ILOs"]["Knowledge"]
+#         # ilos_skill = thisCourse["ILOs"]["Skill"]
+#         # ilos_attitude = thisCourse["ILOs"]["Attitude"]
+#         # textbooks_references = thisCourse["references"]
+        
+#         outputString = f"""---
+# layout: courses
+# permalink: "/courses/semester2/{thisCourse["code"].lower()}/"
+# title: {thisCourse["name"]}
+
+# course_number : {thisCourse["code"].upper()} 
+# course_title : {thisCourse["name"]}
+# credits : {thisCourse["credits"]}
+# core_or_elective : {thisCourse["core/elective"]} 
+# prerequisites : {thisCourse["prerequisites"]}
+# ilos_knowledge : {thisCourse["ILOs"]["Knowledge"]}
+# ilos_skill : {thisCourse["ILOs"]["Skill"]}
+# ilos_attitude : {thisCourse["ILOs"]["Attitude"]}
+# textbooks_references : {thisCourse["references"]}
+# ---"""
+#         print(outputString)
 
         # # write to html file (jekyll frontmatter)
         # # file_url = "../"+f"pages/courses/semester1/{course_code.lower()}.html"
