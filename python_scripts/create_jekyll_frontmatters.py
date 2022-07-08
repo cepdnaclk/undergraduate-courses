cc35f5d05d7182ec09a76b6e64de92dc852c4392
dhmudalige <e18227@eng.pdn.ac.lk>
2022-07-08 22:36:48 +0530

    Update python_scripts/create_jekyll_frontmatters.py

--- python_scripts/create_jekyll_frontmatters.py
+++ python_scripts/create_jekyll_frontmatters.py
@@ -1,5 +1,6 @@
 # Author: E/18/227 Dinuka Mudalige - e18227@eng.pdn.ac.lk
 
+from importlib.resources import contents
 import os
 import json # to get data from _data/courses/semester#.json
 # import requests
@@ -8,48 +9,85 @@ if __name__ == "__main__":
 
     print("Extracting data in _data/courses/semester#.json files to create jekyll frontmatters...")
 
-    # select student from json file
     # jsonPath = f"../_data/courses/semester3.json"
     # dataInJSON = json.load(open(jsonPath))
     dataInJSON = json.load(open("_data/courses/semester3.json"))
 
-    print(type(dataInJSON[0]))
+    for thisCourse in dataInJSON:
+        # print(thisCourse)
 
-#     for thisCourse in dataInJSON:
-#         # print(thisCourse)
-#         course_code = thisCourse["code"]
-#         # course_title = thisCourse["name"]
-#         # course_credits = thisCourse["credits"]
-#         # core_or_elective = thisCourse["core/elective"]
-#         # prerequisties = thisCourse["prerequisites"]
-#         # ilos_knowledge = thisCourse["ILOs"]["Knowledge"]
-#         # ilos_skill = thisCourse["ILOs"]["Skill"]
-#         # ilos_attitude = thisCourse["ILOs"]["Attitude"]
-#         # textbooks_references = thisCourse["references"]
+        course_code = thisCourse["code"]
+        # print(course_code)
+
+        # course_title = thisCourse["name"]
+        # course_credits = thisCourse["credits"]
+        # core_or_elective = thisCourse["type"]
+        # prerequisties = thisCourse["prerequisites"]
+
+        # content = thisCourse["content"]
+        # objectives = thisCourse["objectives"]
+
+        # ilos_knowledge = thisCourse["ILOs"]["Knowledge"]
+        # ilos_skill = thisCourse["ILOs"]["Skill"]
+        # ilos_attitude = thisCourse["ILOs"]["Attitude"]
+
+        modulesList = list()
+        ml = list()
+        modules = thisCourse["modules"]
+
+        for module in modules:
+            ml.append(module["topic"])
+            ml.append(module["description"])
+            ml.append(module["allocation"]["L"])
+            ml.append(module["allocation"]["T"])
+            ml.append(module["allocation"]["P"])
+            ml.append(module["allocation"]["A"])
+            modulesList.append(ml)
+            ml = list()
+
+        # textbooks_references = thisCourse["references"]
+
+        # practicals_marks = thisCourse["marks"]["Practicals"]
+        # assignments_marks = thisCourse["marks"]["Assignments"]
+        # mid_exam_marks = thisCourse["marks"]["Mid-Exam"]
+        # end_exam_marks = thisCourse["marks"]["End-Exam"]
+
+        # lecturers = thisCourse["lecturers"]
         
-#         outputString = f"""---
-# layout: courses
-# permalink: "/courses/semester2/{thisCourse["code"].lower()}/"
-# title: {thisCourse["name"]}
+        outputString = f"""---
+layout: courses
+permalink: "/courses/semester3/{thisCourse["code"].lower()}/"
+title: {thisCourse["name"]}
 
-# course_number : {thisCourse["code"].upper()} 
-# course_title : {thisCourse["name"]}
-# credits : {thisCourse["credits"]}
-# core_or_elective : {thisCourse["core/elective"]} 
-# prerequisites : {thisCourse["prerequisites"]}
-# ilos_knowledge : {thisCourse["ILOs"]["Knowledge"]}
-# ilos_skill : {thisCourse["ILOs"]["Skill"]}
-# ilos_attitude : {thisCourse["ILOs"]["Attitude"]}
-# textbooks_references : {thisCourse["references"]}
-# ---"""
-#         print(outputString)
+course_number : {thisCourse["code"].upper()} 
+course_title : {thisCourse["name"]}
+credits : {thisCourse["credits"]}
+core_or_elective : {thisCourse["type"]} 
+prerequisites : {thisCourse["prerequisites"]}
 
-        # # write to html file (jekyll frontmatter)
-        # # file_url = "../"+f"pages/courses/semester1/{course_code.lower()}.html"
-        # file_url = f"pages/courses/semester2/{course_code.lower()}.html"
-        # os.makedirs(os.path.dirname(file_url), exist_ok=True)
-        # htmlFile = open(file_url, "w")
-        # htmlFile.write(outputString)
-        # htmlFile.close()
-        # print("Create or Update file: " + course_code.lower() + ".html")
-        # print("---------------------------------")
+aims_and_objectives: {thisCourse["objectives"]}
+
+ilos_knowledge : {thisCourse["ILOs"]["Knowledge"]}
+ilos_skill : {thisCourse["ILOs"]["Skill"]}
+ilos_attitude : {thisCourse["ILOs"]["Attitude"]}
+
+modules: {modulesList}
+
+textbooks_references : {thisCourse["references"]}
+
+practicals_marks : {thisCourse["marks"]["Practicals"]}
+assignments_marks : {thisCourse["marks"]["Assignments"]}
+mid_exam_marks : {thisCourse["marks"]["Mid-Exam"]}
+end_exam_marks : {thisCourse["marks"]["End-Exam"]}
+---"""
+        # print(outputString)
+
+        # write to html file (jekyll frontmatter)
+        # file_url = "../"+f"pages/courses/semester3/{course_code.lower()}.html"
+        file_url = f"pages/courses/semester3/{course_code.lower()}.html"
+        os.makedirs(os.path.dirname(file_url), exist_ok=True)
+        htmlFile = open(file_url, "w")
+        htmlFile.write(outputString)
+        htmlFile.close()
+        print("Create or Update file: " + course_code.lower() + ".html")
+        print("---------------------------------")
