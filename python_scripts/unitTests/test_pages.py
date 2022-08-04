c10eb7d7bc1e84964e738373dd7426ff86adcf2e
dhmudalige <e18227@eng.pdn.ac.lk>
2022-08-04 07:24:07 +0530

    Update python_scripts/unitTests/test_pages.py

--- python_scripts/unitTests/test_pages.py
+++ python_scripts/unitTests/test_pages.py
@@ -24,7 +24,10 @@ class Test_Pages(unittest.TestCase):
     def setUp(self):
         self.home = '/'
         self.api = '/api/courses/'
-        self.folderPath = "../../pages/courses"
+        self.semesetersFolderPath = "../courses/"
+        # self.semeseter1FolderPath = "../courses/semeseter1"
+        # self.semeseter2FolderPath = "../courses/semeseter2"
+        # self.semeseter3FolderPath = "../courses/semeseter3"
     
 
     def test_home_page(self):
@@ -33,14 +36,14 @@ class Test_Pages(unittest.TestCase):
     def test_api(self):
         self.assertEqual(get_status_code(self.api), 200, "API not found")
 
-    def test_course_pages(self):
-        allFiles = os.listdir(self.folderPath)
+    # def test_course_pages(self):
+    #     allFiles = os.listdir(self.semeseter3FolderPath)
 
-        for eachFile in allFiles:
-            filePath = self.folderPath + "/" + eachFile
-            permalink = get_permalink(filePath)
-            status_code = get_status_code(permalink)
-            self.assertEqual(status_code, 200, eachFile+" Page not found")
+    #     for eachFile in allFiles:
+    #         filePath = self.semeseter3FolderPath + "/" + eachFile
+    #         permalink = get_permalink(filePath)
+    #         status_code = get_status_code(permalink)
+    #         self.assertEqual(status_code, 200, eachFile+" Page not found")
 
 if __name__ == '__main__':
     unittest.main()
